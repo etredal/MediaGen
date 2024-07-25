3b949025775a63095fc37600412f218ffdc64dfa
Evan <etredal@gmail.com>
2024-07-25 14:55:50 -0700

    Modularize

--- mediaGen.py
+++ mediaGen.py
@@ -5,18 +5,19 @@ from dotenv import load_dotenv
 
 DEBUG = True
 
-# Load environment variables from .env file to get vars
-load_dotenv()
-eleven_labs_api_key = os.getenv('ELEVEN_LABS_API_KEY')
+def GenerateAudio(runID=1, text="Some Subjugates lack a proper understanding of our customs.", voice="Sarah")
+  # Load environment variables from .env file to get vars
+  load_dotenv()
+  eleven_labs_api_key = os.getenv('ELEVEN_LABS_API_KEY')
 
-client = ElevenLabs(
-  api_key=eleven_labs_api_key, # Put your ElevenLabs API key here
-)
+  client = ElevenLabs(
+    api_key=eleven_labs_api_key, # Put your ElevenLabs API key here
+  )
 
-audio = client.generate(
-  text="Some Subjugates lack a proper understanding of our customs.",
-  voice="Sarah",
-  model="eleven_multilingual_v2"
-)
+  audio = client.generate(
+    text=text,
+    voice=voice,
+    model="eleven_multilingual_v2"
+  )
 
-save(audio, "TestElevenLabsOutput.mp3")
+  save(audio, str(runID) + "TestSWT.mp3")
